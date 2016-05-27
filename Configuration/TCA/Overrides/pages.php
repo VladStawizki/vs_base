26fab4b5d9f8ac3e28e5f7b3ce2fdfc4d03a42fc
Vlad Stawizki <vlad.stawizki@gmail.com>
2016-05-27 22:22:32 +0200

    initial commit

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,6 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+        'vs_base',
+        'Configuration/PageTS/Default.txt',
+        'Default Base Configuration'
+);
---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Project base_convert'
+    'description' => 'Add default Configuration for TYPO3 Project',
+    'category' => 'admin',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Vlad Stawizki',
+    'author_email' => 'vlad.stawizki@gmail.com',
+    'author_company' => 'Vlad Stawizki',
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.6.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+);
