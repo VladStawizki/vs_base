337e36e7cc2e4456625e51588077b3ac77c98376
Vlad Stawizki <vlad.stawizki@gmail.com>
2016-05-27 22:38:38 +0200

    add user.ts config

---
+++ Configuration/PageTS/User.ts
@@ -0,0 +1,33 @@
+setup.override {
+    lang = de
+    startModule = web_layout
+    titleLen = 300
+    edit_docModuleUpload = 0
+    rteCleanPasteBehaviour = pasteStructure
+}
+
+setup.fields{
+    emailMeAtLogin.disabled = 1
+    avatar.disabled = 1
+    lang.disabled = 1
+    startModule.disabled = 1
+    thumbnailsByDefault.disabled = 1
+    titleLen.disabled = 1
+
+    edit_RTE.disabled = 1
+    edit_docModuleUpload.disabled = 1
+    showHiddenFilesAndFolders.disabled = 1
+    resizeTextareas_MaxHeight.disabled = 1
+    resizeTextareas_Flexible.disabled = 1
+    copyLevels.disabled = 1
+    recursiveDelete.disabled = 1
+
+    # Rich-Text-Editor ein/ausblenden
+    rteWidth.disabled = 1
+    rteHeight.disabled = 1
+    rteMaxHeight.disabled = 1
+    rteResize.disabled = 1
+    rteCleanPasteBehaviour.disabled = 1
+
+    resetConfiguration.disabled = 1
+}
